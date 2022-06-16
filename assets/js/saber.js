7d5fb5569951ac729b2c4bb2e3859bec7d73eb03
Kioni-Wan Kenobi <69408896+KioniWanKenobi@users.noreply.github.com>
2022-06-15 20:07:25 -0700

    added light and dark mode stylesheets

--- assets/js/saber.js
+++ assets/js/saber.js
@@ -1,3 +1,20 @@
+const btn = document.querySelector(".btn-toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "dark-theme.css";
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "light-theme.css";
+  }
+});
+
 /* create a 150 wide 20 high cylinder named hilt */
 var hilt = document.createElement('div');
 hilt.style.width = '150px';
